3215c7f3291f949a48016b04a727cee875beb802
mihalicman <misjao2005@gmail.com>
2021-04-20 16:06:22 +0300

    Izmaiņas

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.getElementById('jaunaisUzdevums').addEventListener('click', () => {
+    console.log("Poga nospiesta!");
+
+    let popUp = document.getElementById('popUp');
+
+    popUp.style.display = 'block'
+
+})
