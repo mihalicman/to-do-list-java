7e254d1be13ea0e656cff179057442d5fab4acd1
mihalicman <misjao2005@gmail.com>
2021-04-27 15:28:00 +0300

    izmainas

--- scripts.js
+++ scripts.js
@@ -1,8 +1,36 @@
+const POP_UP = document.getElementById('popUp');
+let darbi = []
+
 document.getElementById('jaunaisUzdevums').addEventListener('click', () => {
-    console.log("Poga nospiesta!");
-
-    let popUp = document.getElementById('popUp');
-
-    popUp.style.display = 'block'
+    POP_UP.style.display = 'block'
 
 })
+
+document.getElementById('pievienotUzdevumu').addEventListener('click', () => {
+    POP_UP.style.display = 'none'
+
+    let uzdevumi = {uzdevums: uzdevums.value, termiņš: termiņš.value};
+
+    uzdevums.value = "";
+    termiņš.value = "";
+
+    darbi.push(uzdevumi);
+
+    render();
+})
+
+
+function render() {
+    let biblioteka = document.getElementById('biblioteka');
+
+    for(let i = 0; i < darbi.length; i++){
+        let uzdevumi = ` 
+        <div class='uzdevumi'>
+            <h3>Uzdevums: ${darbi[i].Uzdevums}</h4>
+            <h4>Termiņš: ${darbi[i].Termiņš}</h4>
+        </div>`
+        biblioteka.innerHTML = uzdevumi;  
+    }
+
+
+}
