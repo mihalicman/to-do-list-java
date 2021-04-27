d747113520982ae176c5081d3918a8d445393ed8
mihalicman <misjao2005@gmail.com>
2021-04-27 15:42:01 +0300

    Izmaninas

--- scripts.js
+++ scripts.js
@@ -9,10 +9,10 @@ document.getElementById('jaunaisUzdevums').addEventListener('click', () => {
 document.getElementById('pievienotUzdevumu').addEventListener('click', () => {
     POP_UP.style.display = 'none'
 
-    let uzdevumi = {uzdevums: uzdevums.value, termiņš: termiņš.value};
+    let uzdevumi = {uzdevums: uzdevums.value, termins: termins.value};
 
     uzdevums.value = "";
-    termiņš.value = "";
+    termins.value = "";
 
     darbi.push(uzdevumi);
 
@@ -22,14 +22,15 @@ document.getElementById('pievienotUzdevumu').addEventListener('click', () => {
 
 function render() {
     let biblioteka = document.getElementById('biblioteka');
+    biblioteka.innerHTML = ""
 
     for(let i = 0; i < darbi.length; i++){
         let uzdevumi = ` 
         <div class='uzdevumi'>
-            <h3>Uzdevums: ${darbi[i].Uzdevums}</h4>
-            <h4>Termiņš: ${darbi[i].Termiņš}</h4>
+            <h3>Uzdevums: ${darbi[i].uzdevums}</h4>
+            <h4>Termiņš: ${darbi[i].termins}</h4>
         </div>`
-        biblioteka.innerHTML = uzdevumi;  
+        biblioteka.innerHTML += uzdevumi;
     }
 
 
